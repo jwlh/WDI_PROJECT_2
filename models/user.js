2b7c76ef89c7470ff88f9d4f9d977dee38bf757c
jwlh <jonnyhall@me.com>
2017-10-20 15:25:36 +0100

    added user model and one user to seed file

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+  firstName: {type: String},
+  lastName: {type: String},
+  email: { type: String, required: true },
+  username: { type: String, required: true},
+  password: { type: String, required: true }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
