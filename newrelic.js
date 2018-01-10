f7af5fb0b7572c9f092e4eacdd51a36ffb9e350a
Jonny Hall <jonnyhall@me.com>
2018-01-10 16:03:48 +0000

    added new relic

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Eat Better Pizza'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '88a389a14b9f4174f954d561903d41d6c47a4738',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
